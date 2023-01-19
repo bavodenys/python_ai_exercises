5c7dc7700a571b9e2a832fb999c44b179a24d857
bavo.denys <bavodenys@gmail.com>
2023-01-19 21:08:48 +0100

    try

--- Exercise_2/main.py
+++ Exercise_2/main.py
@@ -5,6 +5,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
+
 # Define model
 class NeuralNetwork(nn.Module):
     def __init__(self):
